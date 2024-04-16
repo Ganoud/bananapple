89c907cd3ebc215469bdc16da32ed91c5d9bfcf2
Spiny150 <matthias.lietard@outlook.fr>
2024-04-16 16:57:09 +0200

    feature(page.php): page inutile pour test si la base de donnée fonctionne

---
+++ page.php
@@ -0,0 +1,32 @@
+<?php
+
+$a = "COUCOU";
+print($a);
+
+$hostname = "localhost";
+$dbUser = "root";
+$dbPassword = ""; 
+$dbName = "example_database";
+$conn = mysqli_connect($hostname, $dbUser, $dbPassword, $dbName);
+if (!$conn){
+    die("something went wrong;");
+}
+
+
+try {
+    $sqlQuery = 'SELECT * FROM users';
+    $result = mysqli_query($conn, $sqlQuery);
+    $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    
+    foreach ($users as $user) {
+?>
+    <p>
+        <?php echo $user['name'] ?>
+    </p>
+<?php
+    }
+}
+catch(Exception $e){
+    echo $e->getMessage();
+}
+?>
