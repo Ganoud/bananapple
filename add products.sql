46bc9b60819e659e46873758f5800d5c71490a74
Spiny150 <matthias.lietard@outlook.fr>
2024-05-11 11:55:28 +0200

    wip

---
+++ add products.sql
@@ -0,0 +1,37 @@
+INSERT INTO products (name, price, description, category_id, discount)
+VALUES 
+("BiPhone X", 999.99, "Le dernier smartphone révolutionnaire de Banana. Avec un écran Super Bi Retina XDR.", 2, 0),
+("BiPhone 11 Pro", 1099.99, "Le téléphone le plus puissant jamais créé par Banana. Doté de trois caméras révolutionnaires.", 2, 0),
+("BiPhone SE (2021)", 499.99, "Un smartphone compact et abordable par Banana, avec la puissance de la puce A13.", 2, 0),
+("BiPhone 13", 1099.99, "Le dernier bijou de Banana, doté d'un design élégant et de la technologie de pointe.", 2, 0),
+("BiPhone 12 Mini", 699.99, "Un téléphone compact mais puissant par Banana, avec un écran Super Bi Retina.", 2, 0),
+("BiPhone XR", 599.99, "Un smartphone coloré et performant par Banana, doté du puissant processeur A12 Bionic.", 2, 0),
+("BiPhone 6S", 299.99, "Un classique revisité par Banana, avec un écran Retina HD et un design emblématique.", 2, 0),
+("BiPhone XS Max", 899.99, "Un téléphone élégant et puissant par Banana, avec un grand écran Super Bi Retina.", 2, 0),
+("BiPhone 8 Plus", 799.99, "Un téléphone phare par Banana, doté d'une double caméra et de la puissance de la puce A11 Bionic.", 2, 0),
+("BiPhone 5S", 199.99, "Un classique intemporel par Banana, avec un design emblématique et des performances fiables.", 2, 0);
+
+INSERT INTO products (name, price, description, category_id, discount)
+VALUES 
+("BiPad Pro", 1299.99, "La tablette la plus avancée de Banana, avec un écran Liquid Bi Retina et une puce M1.", 3, 0),
+("BiPad Air", 899.99, "Une tablette légère et puissante par Banana, avec un écran Liquid Bi Retina et la compatibilité Apple Pencil.", 3, 0),
+("BiPad Mini", 499.99, "Une tablette compacte par Banana, idéale pour la portabilité et la productivité.", 3, 0),
+("BiPad (2021)", 599.99, "La dernière version de la tablette emblématique de Banana, avec un design élégant et des performances supérieures.", 3, 0),
+("BiPad Pro 12.9 (2022)", 1499.99, "La tablette ultime de Banana, avec un écran Liquid Bi XDR et des capacités de studio.", 3, 0),
+("BiPad 10.2 (2022)", 329.99, "La tablette polyvalente de Banana, parfaite pour le travail et les loisirs.", 3, 0),
+("BiPad Pro 11 (2022)", 1099.99, "Une tablette puissante et portable par Banana, conçue pour les professionnels créatifs.", 3, 0),
+("BiPad 9.7 (2021)", 399.99, "Une tablette abordable par Banana, avec un écran Retina et une compatibilité avec Apple Pencil.", 3, 0),
+("BiPad Mini 6 (2021)", 699.99, "La dernière version de la tablette compacte de Banana, avec des performances améliorées et un design élégant.", 3, 0),
+("BiPad Air 5 (2021)", 799.99, "Une tablette élégante et performante par Banana, avec un design fin et des performances améliorées.", 3, 0);
+
+INSERT INTO products (name, price, description, category_id, discount)
+VALUES 
+("BiWatch Series 7", 399.99, "La dernière montre intelligente de Banana, avec un écran plus grand et des fonctionnalités avancées de suivi de la santé.", 4, 0),
+("BiWatch SE", 279.99, "Une montre intelligente abordable par Banana, avec des fonctionnalités avancées de suivi de la forme physique et de la santé.", 4, 0),
+("BiWatch Series 6", 349.99, "Une montre intelligente élégante et puissante par Banana, dotée d'un capteur d'oxygène dans le sang et d'un suivi avancé de la forme physique.", 4, 0),
+("BiWatch Series 5", 299.99, "Une montre intelligente emblématique de Banana, avec un écran Always-On Retina et une boussole intégrée.", 4, 0),
+("BiWatch Series 4", 249.99, "Une montre intelligente avancée de Banana, avec un écran plus grand et des fonctionnalités de suivi de la santé améliorées.", 4, 0),
+("BiWatch Series 3", 199.99, "Une montre intelligente abordable par Banana, idéale pour le suivi de la forme physique et les notifications.", 4, 0),
+("BiWatch SE (2021)", 279.99, "La version économique de la montre intelligente de Banana, avec un design élégant et des fonctionnalités avancées de suivi de la santé.", 4, 0),
+("BiWatch Series 2", 199.99, "Une montre intelligente avancée de Banana, dotée d'un GPS intégré et d'une résistance à l'eau.", 4, 0),
+("BiWatch Series 1", 149.99, "La première montre intelligente de Banana, avec un écran Retina et des fonctionnalités de suivi de la santé.", 4, 0);
--- panier.php
+++ panier.php
@@ -7,6 +7,9 @@
     <link rel="icon" href="logo.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -14,4 +17,28 @@
     require "general.php";
     require "menu.php";
     ?>
+    <h2 class="page-title">Votre panier</h2>
+    <hr>
+
+    <?php
+    $request = "SELECT * FROM products WHERE id=25";
+    $statement = $conn->prepare($request);
+    $statement->execute();
+    $product = $statement->get_result()->fetch_assoc();
+
+    ?>
+
+    <div class=""></div>
+    <div class="cart-items-container">
+        <div class="cart-item">
+            <?php echo $product["name"] ?>
+        </div>
+        <div>
+
+        </div>
+    </div>
+    <div class="cart-summary">
+
+    </div>
+
 </body>
