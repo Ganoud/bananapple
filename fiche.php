045aba407ccb3c0c09a242e259d2359c76eec604
Nicolas <nicolas.famille.pajot@gmail.cop>
2024-05-11 12:06:51 +0200

    j'ai ajouter une partie dans le css et j'ai ajouter des lignes dans le fiche.php

---
+++ fiche.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Banana</title>
+    <link rel="icon" href="logo.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+</head>
+
+<body>
+    <?php
+    require "general.php";
+    require "menu.php";
+    ?>
+
+    <div class="card">
+        <div class="card-image"></div>
+        <div class="card-content">
+            <h2 class="card-title">Titre de la carte 2</h2>
+            <p class="card-description">Description de la carte.</p>
+        </div>
+    </div>
+
+
+</body>
