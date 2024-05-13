772742033f1a447c4e1f19c93a4567e8175bde5b
Nicolas <nicolas.famille.pajot@gmail.cop>
2024-05-14 00:01:28 +0200

    tu ne m'en voudra pas mais j'ai copié le code de connexion.php dans un card dans compte.php parce que je voulais le faire comme ceci mais je ne sais pas comment faire autrement et j'ai un peu modifié le css faut juste que je rajoute le lien vers la page d'inscription

--- compte.php
+++ compte.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Catégorie</title>
+    <title>Compte</title>
     <link rel="icon" href="logo.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
@@ -12,15 +12,41 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Roboto:wght@100;300;400&display=swap"
         rel="stylesheet">
-
-    
 </head>
 
 <body>
     <?php
-    require "serveurUtils.php";
+    require "serverUtils.php";
     require "menu.php";
     ?>
-    <h2 class="page-title">Votre panier</h2>
+    <h2 class="page-title">Connectez-vous</h2>
     <hr>
+    <div class="card-container">
+        <div class="card">
+            <div class="card-content-connexion">
+                <h2 class="card-title">Se connecter</h2>
+                <div class="inscription-box">
+                    <img src="logopetit.png" alt="">
+                    <h2 class="form-element">Connexion</h2>
+                    <form action="connexion_traitement.php" method="post">
+                        <div class="form-element">
+                            <label for="email">
+                                <span><i class="fa fa-envelope-o"></i></span> E-mail :
+                            </label><br>
+                            <input type="email" id="email" name="email" required><br>
+                        </div>
+                        <div class="form-element">
+                            <label for="password">
+                                <span><i class="fa fa-unlock-alt"></i></span> Mot de passe :
+                            </label><br>
+                            <input type="password" id="password" name="password" required><br>
+                        </div>
+                        <div class="form-element">
+                            <input type="submit" value="Se connecter" class="button">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
