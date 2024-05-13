962df157349d9e3e6293d231e39c04eb4bc580f9
Nicolas <nicolas.famille.pajot@gmail.cop>
2024-05-13 08:09:33 +0200

    ajout du modal et du javascript

--- script.js
+++ script.js
@@ -39,3 +39,65 @@ function showSlides(n) {
     dots[slideIndex - 1].className += " active";
 }
 
+function openModal(productName, productPrice, images) 
+{
+    var modal = document.getElementById('MyModal');
+    var carousel = document.getElementById('carousel');
+    var thumbnailsContainer = document.querySelector('.carousel-thumbnails');
+    var productNameElement = document.getElementById('productName');
+    var productPriceElement = document.getElementById('productPrice');
+
+    // Mettre à jour le contenu du modal
+    productNameElement.innerText = productName;
+    productPriceElement.innerText = 'Prix: ' + productPrice + ' €';
+    carousel.innerHTML = '';
+    thumbnailsContainer.innerHTML = '';
+
+    // Ajouter les images au carousel et les vignettes
+    images.forEach(function(image, index) {
+        var carouselItem = document.createElement('div');
+        carouselItem.classList.add('.carousel-item');
+        carouselItem.innerHTML = '<img src="' + image + '" alt="Image ' + (index + 1) + '">';
+        if (index === 0)w 
+        {
+            carouselItem.classList.add('active');
+        }
+        
+
+        var thumbnail = document.createElement('img');
+        thumbnail.src = image;
+        thumbnail.alt = 'Image ' + (index + 1);
+        thumbnail.onclick = function() 
+        {
+            showImage(index);
+        };
+        thumbnailsContainer.appendChild(thumbnail);
+
+        carousel.appendChild(carouselItem);
+    });
+
+    modal.style.display = 'block';
+}
+
+// Fonction pour fermer le modal
+function closeModal() 
+{
+    var modal = document.getElementById('MyModal');
+    modal.style.display = 'none';
+}
+
+// Fonction pour afficher une image spécifique dans le carousel
+function showImage(index) 
+{
+    var carouselItems = document.querySelectorAll('.carousel-item');
+    carouselItems.forEach(function(item, i) {
+        if (i === index) 
+        {
+            item.classList.add('active');
+        } else 
+        {
+            item.classList.remove('active');
+        }
+    });
+}
+
